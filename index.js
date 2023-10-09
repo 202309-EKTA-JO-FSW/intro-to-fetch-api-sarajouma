d99e57fe1df14c6a39ba3ae3c1458c8ecc292c80
sarajouma <144836947+sarajouma@users.noreply.github.com>
2023-10-09 18:37:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Please read the README.md to understand what to do. Happy Coding !
+const fetchUserData = async() => {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const usersData = await response.json();
+        const modifiedUsers = usersData.map(user => ({
+            Name: user.name,
+            Username: user.username,
+            Email: user.email,
+            Address: user.address.street, 
+            Phone: user.phone,
+            Website: user.website,
+            Company: user.company.name 
+          }));
+        console.log("Modified Users List:",modifiedUsers);
+    
+        const filterUsersWithBizEmail = modifiedUsers.filter(user => user.Email.endsWith('.biz'));
+        console.log("Filtered Users List:",filterUsersWithBizEmail);
+    
+        modifiedUsers.sort((a, b) => a.Name.localeCompare(b.Name));
+        console.log("Sorted Users List:",modifiedUsers);
+    }
+    catch(error)
+    {
+        return console.log("An error has occured")
+    }
+}
+
+fetchUserData();
